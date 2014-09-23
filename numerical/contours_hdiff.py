c2e7881dde2051ccc436ddf12b609fbb5a178bb5
djarecka <djarecka@gmail.com>
2014-09-22 23:19:46 -0600

    calculating h error for all domain

--- numerical/contours_hdiff.py
+++ numerical/contours_hdiff.py
@@ -29,17 +29,14 @@ def main(dir, lamb_x0=2., lamb_y0=1., time=7, dt=0.01, dx=0.05, xy_lim=10,
     x_range = y_range = np.arange(-xy_lim, xy_lim, dx)
     time_str = str(int(time/dt))
     h_m, px_m, py_m = reading_modeloutput(dir+"spreading_drop_2delipsa_" + casename + ".out/timestep0000000" + time_str + '.h5')
-    #calculating velocity from momentum, only for the droplet area - using numpy mask
-    h_m = np.ma.masked_less(h_m, eps)
-    #calculating the analytical solution, using the same mask
+    #calculating the analytical solution
     lamb_ar = an_eq.d2_el_lamb_lamb_t_evol(time, lamb_x0, lamb_y0)
     h_an = an_eq.d2_el_height_plane(lamb_ar[0], lamb_ar[2], x_range, y_range)
-    h_an = np.ma.masked_less(h_an, eps)
     #the error is normalized by initial height of the drop
     h0_max = 1./ (lamb_x0 * lamb_y0)
     h_diff = abs(h_m - h_an)/h0_max
     print "max of abs(h_m-h_an), h_an.max, h_m.max", abs(h_diff).max(), h_an.max(), h_m.max()
-    levels_diff = np.linspace(0, 2.e-3, 4)
+    levels_diff = np.linspace(1.e-7, 2.e-3, 4)
     plt.figure(1, figsize = (3.,3.))
     ax = plt.subplot(1,1,1,aspect='equal')
     contour_plot(ax, h_diff, x_range, y_range, levels_var = levels_diff)
